3a7c787718e4951656a50ccdb69c0e318ce5efd5
zheliu137 <zheliu137@gmail.com>
2024-05-21 13:09:38 -0700

    update

--- 2.1-cuda_in_mpifort/test__genmod.f90
+++
@@ -1,9 +0,0 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Tue Mar 26 17:18:27 2024
-        ! This source file is for reference only and may not completely
-        ! represent the generated interface used by the compiler.
-        MODULE TEST__genmod
-          INTERFACE 
-            SUBROUTINE TEST
-            END SUBROUTINE TEST
-          END INTERFACE 
-        END MODULE TEST__genmod
